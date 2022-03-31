f1eb22c7324ee4572e557007a413ba739c238327
az-P1-Win10 <yzhaocl@connect.ust.hk>
2022-03-31 20:27:37 +0800

    Test board's functionality

---
+++ include/test_board.hpp
@@ -0,0 +1,12 @@
+#ifndef TEST_BOARD
+#define TEST_BOARD
+
+#include <Arduino.h>
+
+void ledBlink(void);
+void ledBlinkWithoutDelay(void);
+void ledFade(void);
+void ledDimmer(void);
+void printASCII(void);
+
+#endif
