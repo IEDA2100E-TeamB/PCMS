243badc57d524a1b10dfb7a9a2d0faa5b304c09a
az-P1-Win10 <yzhaocl@connect.ust.hk>
2022-03-31 20:29:57 +0800

    Finish board testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
+#include "test_board.hpp"
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+	// put your setup code here, to run once:
+	pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(9600);
+	Serial.println("serial setup done!");
+	printASCII();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	// put your main code here, to run repeatedly:
+	ledBlinkWithoutDelay();
 }
