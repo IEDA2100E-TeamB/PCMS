eb33c4b4c3b5f68bde94d05d5e0ecb0428350c59
az-P1-Win10 <yzhaocl@connect.ust.hk>
2022-04-04 01:58:41 +0800

    Comment out TEST code for later dev

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,21 @@
 
 void setup()
 {
+	// -------- TEST BOARD start --------
+	// pinMode(LED_BUILTIN, OUTPUT);
+	// Serial.begin(115200);
+	// Serial.println("serial setup done!");
+	// printASCII();
+	// -------- TEST BOARD end --------
+
 	// put your setup code here, to run once:
-	pinMode(LED_BUILTIN, OUTPUT);
-	Serial.begin(115200);
-	Serial.println("serial setup done!");
-	printASCII();
 }
 
 void loop()
 {
+	// -------- TEST BOARD start --------
+	// ledBlinkWithoutDelay();
+	// -------- TEST BOARD end --------
+
 	// put your main code here, to run repeatedly:
-	ledBlinkWithoutDelay();
 }
