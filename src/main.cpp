b71b445e661a5364927283b12817c95358b6fa75
az-P1-Win10 <yzhaocl@connect.ust.hk>
2022-04-04 01:54:26 +0800

    Change baud rate

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ void setup()
 {
 	// put your setup code here, to run once:
 	pinMode(LED_BUILTIN, OUTPUT);
-	Serial.begin(9600);
+	Serial.begin(115200);
 	Serial.println("serial setup done!");
 	printASCII();
 }
