344f7320ce71047a3ac4fe7eba063e1df5dc9bce
az-P1-Win10 <yzhaocl@connect.ust.hk>
2022-04-04 19:57:27 +0800

    Finish testing BME280

--- src/test_board.cpp
+++ src/test_board.cpp
@@ -1,53 +1,50 @@
-#include "test_board.hpp"
+#include "test_board.h"
 
 // variables for ledBlinkWithoutDelay()
-int ledState = LOW; // ledState used to set the LED
+static uint8_t ledState = LOW;
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
-uint32_t previousMillis = 0; // will store last time LED was updated
-constexpr long ledBlinkInterval = 1000; // interval at which to blink (milliseconds)
+static uint32_t prevMillis = 0; // last time LED status was updated
+static constexpr uint32_t BLINK_INTERVAL = 1000; // interval at which to blink (milliseconds)
 
 // variables for ledFade()
-int ledBrightness = 0; // how bright the LED is
-int ledFadeAmount = 5; // how many points to fade the LED by
+static int16_t ledBrightness = 0; // how bright the LED is
+static int16_t ledFadeAmount = 5; // how many points to fade the LED by
 
 // variables for serialEvent)
-String inputString = ""; // a String to hold incoming data
-bool stringComplete = false; // whether the string is complete
+static String inputString = ""; // a String to hold incoming data
+static bool stringComplete = false; // whether the string is complete
 
-// EXAMPLE OF TESTS
-// void setup()
-// {
-// 	// put your setup code here, to run once:
+// -------- EXAMPLE OF TEST SETUP --------
+void test_setup()
+{
+	// put your setup code here, to run once:
+	Serial.begin(115200);
+	// while (!Serial) {
+	// 	; // wait for serial port to connect. Needed for native USB port only
+	// }
+	Serial.println("serial setup done!");
+	pinMode(LED_BUILTIN, OUTPUT);
+	inputString.reserve(200); // reserve 200 bytes for the inputString:
+}
 
-// 	Serial.begin(9600);
-// 	// while (!Serial) {
-// 	// 	; // wait for serial port to connect. Needed for native USB port only
-// 	// }
-// 	Serial.println("serial setup done!");
-
-// 	pinMode(LED_BUILTIN, OUTPUT);
-
-// 	inputString.reserve(200); // reserve 200 bytes for the inputString:
-// }
-
-// void loop()
-// {
-// 	// put your main code here, to run repeatedly:
-
-// 	// ledBlink();
-// 	// ledBlinkWithoutDelay();
-// 	ledFade();
-// 	// ledDimmer();
-// 	// printASCII();
-// 	// print the string when a newline arrives:
-// 	if (stringComplete) {
-// 		Serial.println(inputString);
-// 		// clear the string:
-// 		inputString = "";
-// 		stringComplete = false;
-// 	}
-// }
+// -------- EXAMPLE OF TEST LOOP --------
+void test_loop()
+{
+	// put your main code here, to run repeatedly:
+	// ledBlink();
+	// ledBlinkWithoutDelay();
+	ledFade();
+	// ledDimmer();
+	// printASCII();
+	// print the string when a newline arrives:
+	if (stringComplete) {
+		Serial.println(inputString);
+		// clear the string:
+		inputString = "";
+		stringComplete = false;
+	}
+}
 
 void ledBlink(void)
 {
@@ -63,19 +60,17 @@ void ledBlinkWithoutDelay(void)
 	// check to see if it's time to blink the LED; that is, if the difference
 	// between the current time and last time you blinked the LED is bigger than
 	// the interval at which you want to blink the LED.
-	uint32_t currentMillis = millis();
+	uint32_t currMillis = millis();
 
-	if (currentMillis - previousMillis >= ledBlinkInterval) {
+	if (currMillis - prevMillis >= BLINK_INTERVAL) {
 		// save the last time you blinked the LED
-		previousMillis = currentMillis;
-
+		prevMillis = currMillis;
 		// if the LED is off turn it on and vice-versa:
 		if (ledState == LOW) {
 			ledState = HIGH;
 		} else {
 			ledState = LOW;
 		}
-
 		// set the LED with the ledState of the variable:
 		digitalWrite(LED_BUILTIN, ledState);
 	}
@@ -85,23 +80,19 @@ void ledFade(void)
 {
 	// set the brightness of pin 9:
 	// analogWrite(LED_BUILTIN, ledBrightness);
-
 	// change the brightness for next time through the loop:
 	ledBrightness = ledBrightness + ledFadeAmount;
-
 	// reverse the direction of the fading at the ends of the fade:
 	if (ledBrightness <= 0 || ledBrightness >= 255) {
 		ledFadeAmount = -ledFadeAmount;
 	}
-
 	// wait for 20 milliseconds to see the dimming effect
 	delay(20);
 }
 
 void ledDimmer(void)
 {
-	byte brightness;
-
+	uint8_t brightness;
 	// check if data has been sent from the computer:
 	if (Serial.available()) {
 		// read the most recent byte (which will be from 0 to 255):
@@ -122,7 +113,7 @@ void printASCII(void)
 	// for example, '!' is the same as 33, so you could also use this:
 	// int thisByte = '!';
 
-	for (int thisByte = 33; thisByte < 127; thisByte++) {
+	for (uint8_t thisByte = 33; thisByte < 127; thisByte++) {
 		// prints value unaltered, i.e. the raw binary version of the byte.
 		// The Serial Monitor interprets all bytes as ASCII, so 33, the first number,
 		// will show up as '!'
