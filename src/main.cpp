c4c95eaf9494a1e544e4c5d12a1de8467c14c927
az-P1-Win10 <yzhaocl@connect.ust.hk>
2022-05-15 14:07:02 +0800

    Change to thread

--- src/main.cpp
+++ src/main.cpp
@@ -2,64 +2,119 @@
 #include "test_board.h"
 
 // ======== INCLUDES ========
-// ======== SENSOR SYSTEM ========
+// local system
 #include "i2c_scanner.h"
 #include "i2c_bme280.h"
 #include "i2c_mpu6050.h"
 #include "adc_light.h"
 #include "gpio_hall.h"
 #include "gpio_active_buzzer.h"
-// ======== GPS MODULE ========
-#include "PCMS_GPS_GPRS.hpp"
-// ======== GATEWAY ========
+// gateway communication
 #include <WiFi.h>
 #include "socket_conn.hpp"
 #include "sensor_data.hpp"
 #include "unix_time.hpp"
+// server communication
+#include "PCMS_GPS_GPRS.hpp"
 
 // ======== DEFINES ========
-// please write down the pins used in ascending order:
-#define ADC_LIGHT_SENSOR 4
-#define DIN_HALL_SENSOR 12
-#define DOU_ACTIVE_BUZZER 18
-#define I2C_SDA 21
-#define I2C_SCL 22
+
+// ======== PIN USED ========
+// debugging
+static const uint8_t SERIAL0_RX = 3;
+static const uint8_t SERIAL0_TX = 1;
+// local system
+static const uint8_t ADC_LIGHT_SENSOR = 4;
+static const uint8_t DIN_HALL_SENSOR = 12;
+static const uint8_t DOU_ACTIVE_BUZZER = 18;
+static const uint8_t I2C_SDA = 21;
+static const uint8_t I2C_SCL = 22;
+// server communication
+static const uint8_t SERIAL2_RX = 16;
+static const uint8_t SERIAL2_TX = 17;
 
 // ======== VARIABLES ========
-bool A9G_state = false, disconnected_gateway = true;
-String GPS_data = "";
-
-bool isWiFiInitSuccess = false;
-bool isSocketConnectSuccess = false;
-bool isTimeSyncSuccess = false;
+// gateway communication
+// static bool isWiFiInitSuccess = false;
+// static bool isSocketConnectSuccess = false;
+// static bool isTimeSyncSuccess = false;
+// server communication
+static bool A9G_state = false, disconnected_gateway = true;
+static String GPS_data = "";
 
 // ======== FUNCTION PROTOTYPES ========
+void system_task(void *pvParameters);
+void gatewayCommunication_task(void *pvParameters);
+void serverCommunication_task(void *pvParameters);
 
 void setup()
 {
 	// put your setup code here, to run once:
 	Serial.begin(115200);
-	Serial2.begin(115200);
 
-	// ======== SENSOR SYSTEM ========
+	// ======== local system init ========
+	// ======== gateway communication init ========
+	// !!! we init this part first
+	if (wifi_init() != true) {
+		return;
+	}
+	if (sync_time() != true) {
+		return;
+	}
+	if (socket_connect() != true) {
+		return;
+	}
+
 	// i2cScannerSetup();
 	bme280_setup();
 	mpu6050_setup();
 	my_aBuzzer_init();
 
-	// ======== GPS MODULE ========-
-	sendCommandToA9G("AT", 5, "OK");
+	// ======== server communication init ========
+	// !!! we skip server communication init and ONLY init it when neccesary
+	// Serial2.begin(115200);
+	// sendCommandToA9G("AT", 5, "OK");
 
-	// ======== GATEWAY ========
-	isWiFiInitSuccess = wifi_init();
-	if (!isWiFiInitSuccess) {
+	// ======== threads init ========
+	static TaskHandle_t task_system = NULL;
+	static TaskHandle_t task_gatewayCommunication = NULL;
+	static TaskHandle_t task_serverCommunication = NULL;
+	BaseType_t xReturned = pdFAIL;
+	//create a task that will be executed in the function, with priority 1 and executed on core 0
+	xReturned = xTaskCreatePinnedToCore(system_task, /* Task function. */
+					    "System", /* name of task. */
+					    10000, /* Stack size of task */
+					    NULL, /* parameter of the task */
+					    1, /* priority of the task */
+					    &task_system, /* Task handle to keep track of created task */
+					    0); /* pin task to core 0 */
+	if (xReturned != pdPASS) {
 		return;
 	}
-	isTimeSyncSuccess = sync_time();
-	if (!isTimeSyncSuccess) {
+	delay(500);
+	xReturned = pdFAIL;
+	//create a task that will be executed in the function, with priority 1 and executed on core 1
+	xTaskCreatePinnedToCore(gatewayCommunication_task, /* Task function. */
+				"Gateway Communication", /* name of task. */
+				10000, /* Stack size of task */
+				NULL, /* parameter of the task */
+				1, /* priority of the task */
+				&task_gatewayCommunication, /* Task handle to keep track of created task */
+				1); /* pin task to core 1 */
+	if (xReturned != pdPASS) {
 		return;
 	}
-	isSocketConnectSuccess = socket_connect();
+	delay(500);
+	// !!! not creating it now
+	//create a task that will be executed in the function, with priority 1 and executed on core 1
+	// xTaskCreatePinnedToCore(serverCommunication_task, /* Task function. */
+	// 			"Task2", /* name of task. */
+	// 			10000, /* Stack size of task */
+	// 			NULL, /* parameter of the task */
+	// 			1, /* priority of the task */
+	// 			&task_serverCommunication, /* Task handle to keep track of created task */
+	// 			1); /* pin task to core 1 */
+	// delay(500);
 }
 
 void loop()
@@ -69,45 +124,89 @@ void loop()
 	// ======== TEST BOARD end ========
 
 	// put your main code here, to run repeatedly:
+}
 
-	// ======== SENSOR SYSTEM ========
-	// i2cScannerLoop();
-	bme280_print();
-	mpu6050_print();
-	my_light_print();
-	// my_aBuzzer_alarm();
-
-	// ======== GPS MODULE ========--
-	if (disconnected_gateway == true) {
-		//when A9G off
-		if (A9G_state == false) {
-			//sendCommandToA9G("AT+SLEEP=0",3,"OK"); //wake A9G up
-			//delay(2000);
-			connect_mqqt_broker();
-			start_GPS();
-			A9G_state = true;
-		}
-		//when A9G is already operating
-		else {
-			get_GPS_data();
-
-			//Serial.print("GPS DATA: ");
-			// Serial.println(GPS_data);
-			//ShowSerialData();
-			Serial.println();
-			send_JSON_data();
-
-			delay(1000);
-		}
-	} else {
-		if (A9G_state == true) {
-			terminate_GPS();
-			terminate_broker_connection();
-			//sendCommandToA9G("AT+SLEEP=2",3,"OK"); //low power consumption mode
-			A9G_state = false;
-		}
-		//send data to gateway device
+void system_task(void *pvParameters)
+{
+	for (;;) {
+		// --Task application code here.--
+		// i2cScannerLoop();
+		bme280_print();
+		mpu6050_print();
+		my_light_print();
+		// my_aBuzzer_alarm();
 	}
-	Serial.println();
-	delay(1000);
+
+	/* Tasks must not attempt to return from their implementing
+        function or otherwise exit.  In newer FreeRTOS port
+        attempting to do so will result in an configASSERT() being
+        called if it is defined.  If it is necessary for a task to
+        exit then have the task call vTaskDelete( NULL ) to ensure
+        its exit is clean. */
+	vTaskDelete(NULL);
+}
+
+void gatewayCommunication_task(void *pvParameters)
+{
+	for (;;) {
+		// --Task application code here.--
+		SensorData sensorData(1.1, 1.1, 1.1, 1.1, 1.1, 1.1, String("test"), String(get_time()));
+		socket_send_sensor_data(&sensorData);
+		delay(10000);
+	}
+
+	/* Tasks must not attempt to return from their implementing
+        function or otherwise exit.  In newer FreeRTOS port
+        attempting to do so will result in an configASSERT() being
+        called if it is defined.  If it is necessary for a task to
+        exit then have the task call vTaskDelete( NULL ) to ensure
+        its exit is clean. */
+	vTaskDelete(NULL);
+}
+
+void serverCommunication_task(void *pvParameters)
+{
+	for (;;) {
+		// --Task application code here.--
+		if (disconnected_gateway == true) {
+			//when A9G off
+			if (A9G_state == false) {
+				//sendCommandToA9G("AT+SLEEP=0",3,"OK"); //wake A9G up
+				//delay(2000);
+				connect_mqqt_broker();
+				start_GPS();
+				A9G_state = true;
+			}
+			//when A9G is already operating
+			else {
+				get_GPS_data();
+
+				//Serial.print("GPS DATA: ");
+				// Serial.println(GPS_data);
+				//ShowSerialData();
+				Serial.println();
+				send_JSON_data();
+
+				delay(1000);
+			}
+		} else {
+			if (A9G_state == true) {
+				terminate_GPS();
+				terminate_broker_connection();
+				//sendCommandToA9G("AT+SLEEP=2",3,"OK"); //low power consumption mode
+				A9G_state = false;
+			}
+			//send data to gateway device
+		}
+		Serial.println();
+		delay(1000);
+	}
+
+	/* Tasks must not attempt to return from their implementing
+        function or otherwise exit.  In newer FreeRTOS port
+        attempting to do so will result in an configASSERT() being
+        called if it is defined.  If it is necessary for a task to
+        exit then have the task call vTaskDelete( NULL ) to ensure
+        its exit is clean. */
+	vTaskDelete(NULL);
 }
