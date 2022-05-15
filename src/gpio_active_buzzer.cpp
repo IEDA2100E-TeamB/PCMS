1b36f09f973e6eff9df1709b35cf82e888b870f5
az-P1-Win10 <yzhaocl@connect.ust.hk>
2022-05-16 03:28:21 +0800

    Update the logic and check for thresholds

--- src/gpio_active_buzzer.cpp
+++ src/gpio_active_buzzer.cpp
@@ -9,7 +9,7 @@ bool triggerAlarm = false;
 
 static bool isAlarming = false;
 static uint32_t startMillis = 0;
-static constexpr uint32_t ALARM_DELAY = 5000;
+static constexpr uint32_t ALARM_DELAY = 120000;
 
 void my_aBuzzer_init()
 {
@@ -19,21 +19,25 @@ void my_aBuzzer_init()
 
 void my_aBuzzer_alarm()
 {
-	if (triggerAlarm) {
-		if (isAlarming != true) {
-			// trigger alarm
-			isAlarming = true;
-			startMillis = millis();
-			// digitalWrite(PIN_GPIO_ACTIVE_BUZZER, HIGH);
-			digitalWrite(18, HIGH);
-		} else {
-			uint32_t currMillis = millis();
-			if (currMillis - startMillis >= ALARM_DELAY) {
-				// stop alarm
-				isAlarming = false;
-				// digitalWrite(PIN_GPIO_ACTIVE_BUZZER, LOW);
-				digitalWrite(18, LOW);
-			}
-		}
-	}
+	// if (triggerAlarm) {
+	// 	if (isAlarming != true) {
+	// 		// trigger alarm
+	// 		isAlarming = true;
+	// 		startMillis = millis();
+	// 		// digitalWrite(PIN_GPIO_ACTIVE_BUZZER, HIGH);
+	// 		digitalWrite(18, HIGH);
+	// 	} else {
+	// 		uint32_t currMillis = millis();
+	// 		if (currMillis - startMillis >= ALARM_DELAY) {
+	// 			// stop alarm
+	// 			isAlarming = false;
+	// 			// digitalWrite(PIN_GPIO_ACTIVE_BUZZER, LOW);
+	// 			digitalWrite(18, LOW);
+	// 		}
+	// 	}
+	// }
+
+	digitalWrite(18, HIGH);
+	delay(ALARM_DELAY);
+	digitalWrite(18, LOW);
 }
